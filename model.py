e9508e2e719d78f8ab4c04b568ab107defa1dd77
Yuriy Nazarov <nazarov.yuriy.pavlovich@gmail.com>
2023-03-28 17:42:57 +0200

    Add geglu

--- model.py
+++ model.py
@@ -23,6 +23,11 @@ def new_gelu(x):
     """
     return 0.5 * x * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (x + 0.044715 * torch.pow(x, 3.0))))
 
+
+def geglu(x):
+    x, gate = x.chunk(2, dim=-1)
+    return F.gelu(gate) * x
+
 class LayerNorm(nn.Module):
     """ LayerNorm but with an optional bias. PyTorch doesn't support simply bias=False """
 
@@ -87,13 +92,13 @@ class MLP(nn.Module):
 
     def __init__(self, config):
         super().__init__()
-        self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
-        self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
+        self.c_fc    = nn.Linear(config.n_embd, 6 * config.n_embd, bias=config.bias)
+        self.c_proj  = nn.Linear(3 * config.n_embd, config.n_embd, bias=config.bias)
         self.dropout = nn.Dropout(config.dropout)
 
     def forward(self, x):
         x = self.c_fc(x)
-        x = new_gelu(x)
+        x = geglu(x)
         x = self.c_proj(x)
         x = self.dropout(x)
         return x
